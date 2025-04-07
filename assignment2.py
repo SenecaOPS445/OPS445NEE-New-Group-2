6167c583f48dac1c97579efb61ffcefcbe9a9101
Jakob Tuma <jtuma@myseneca.ca>
2025-04-06 22:20:08 -0400

    I think the script is finished and ready for tomorrow. I added the argparse module and comments. I also did some editing on Sultan's code. The IP error checking is now a single function and I took out part of the subnet error checking function and it now only accepts network IDs. I did this because the error subnet function wasn't working properly and it would be too complicated given the time constraint to check the subnet given a non network id IP address. Let me know what you think and we should merge it with main before tomorrow's class. Also, did anyone do the Readme.md?

--- assignment2.py
+++ assignment2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-import sys
 import os
 import ipaddress
+import argparse
 
+'''This program takes an IP address or network address as an argument and pings the address or all addresses in the given subnet
+If the user specifies an IP address as the second argument, it is interpreted as a range and all the addresses are pinged'''
 def ping_ip(ip):
     '''Pings an IP address and prints if it's up or down'''
     if os.name == "nt":  # Windows
@@ -27,22 +29,29 @@ def check_ip_range(start_ip, end_ip):
         if len(ip_list) > 0:
             print(f"Created list with {len(ip_list)} addresses.")
             print("Starting ping test for all IPs in the range...")
-            Total_ip_up = 0
-            Total_ip_down = 0
+            # counters to check the total number of successful pings and failed pings
+            total_ip_up = 0
+            total_ip_down = 0
 
             # Ping each IP in the list
             for ip in ip_list:
                 if ping_ip(ip):
-                    Total_ip_up +=1
+                    # if the ping is successful, the counter bellow goes up by one
+                    total_ip_up +=1
                 else:
-                    Total_ip_down +=1
-            print (f"{Total_ip_up} IPs are up")
-            print (f"{Total_ip_down} IPs are down")
+                    # if the ping failed, the other counter goes up
+                    total_ip_down +=1
+                    # these results are printed
+            print (f"{total_ip_up} IPs are up")
+            print (f"{total_ip_down} IPs are down")
         else:
             print("No IPs to ping due to empty range")
-            
+        
+        '''If there is a problem converting the input to an IP address the code bellow will print an error message'''
+
     except ValueError as e:
         print(f"Error: Invalid IP address format - {e}")
+        ''' if there is any other problem with the code, the line bellow will print an error message'''
     except Exception as e:
         print(f"Error: {e}")
 
@@ -56,17 +65,18 @@ def check_ip_subnet(subnet):
         if len(ip_list) > 0:
             print(f"Created list with {len(ip_list)} addresses.")
             print("Starting ping test for all IPs in the range...")
-            Total_ip_up = 0
-            Total_ip_down = 0
+            total_ip_up = 0
+            total_ip_down = 0
             
             # Ping each IP in the list
             for ip in ip_list: 
                 if ping_ip(ip):
-                    Total_ip_up +=1
+                    # if the ping is successful, the total_ip_up 
+                    total_ip_up +=1
                 else:
-                    Total_ip_down +=1
-            print (f"{Total_ip_up} IPs are up")
-            print (f"{Total_ip_down} IPs are down")
+                    total_ip_down +=1
+            print (f"{total_ip_up} IPs are up")
+            print (f"{total_ip_down} IPs are down")
         else:
             print("No IPs to ping due to empty range")
             
@@ -80,83 +90,98 @@ def iprangetolist(startip, endip):
     '''Takes 2 ip addresses and returns a list of addresses in that range'''
     iplist = [startip] # initializes the list with the first ip address
     ip = startip
-    while ip != endip: #iterrates through ip addresses until it reaches the end address
+    while ip < endip: #iterrates through ip addresses until it reaches the end address
         ip = str(ipaddress.IPv4Address(ip) + 1)
         iplist.append(ip)
     return iplist
 
 def ipsubnettolist(subnet):
     '''Takes an ip with a subnet and converts it to a list of host addesses in the subnet'''
-    iplist = list(ipaddress.IPv4Network(subnet, strict=False).hosts()) # Creates a list of host addresses
+    iplist = list(ipaddress.IPv4Network(subnet, strict=True).hosts()) # Creates a list of host addresses
     iplist = list(map(str, iplist)) # Converts the items in the list to strings
     return iplist
 
-def validate_ip(ip_to_check):
+'''Function to check if IP addresses are valid
+Takes one mandatory argument: an IP address and another mandatory argument: a second IP address'''
+def validate_ip(first_ip, last_ip = None):
     try:
-        ipaddress.IPv4Address(ip_to_check)
-        return True
-    
-    except ipaddress.AddressValueError as e:
-        print (f"Error: Invalid IP address - {e})")
-        return False
-    except ValueError as e:
-        print (f"Error: {e}")
-        return False
-
-def validate_ip_in_range(range_start, range_end):
-    try:    
-        start_ip = ipaddress.IPv4Address(range_start)
-        end_ip = ipaddress.IPv4Address(range_end)
-            
-        if start_ip > end_ip:
-                start_ip = end_ip
-                end_ip = start_ip
-                print("Start IP is > End IP")
-                print("Swaping Start IP and End IP")
+        # first, the function checks if a second IP address was passed in
+        if last_ip:
+            # if yes, it checks that the second IP address is greater than the first IP address preventing errors later in the program
+            if ipaddress.IPv4Address(last_ip) > ipaddress.IPv4Address(first_ip):
+                # if the condition is true, the function returns true
                 return True
-        return True
-                
-    except ipaddress.AddressValueError as e:
-        print (f"Error: Invalid IP address - {e})")
-        return False
-    except ValueError as e:
-        print (f"Error: {e}")
-        return False
-    
-def validate_ip_in_subnet(ip_to_check, subnet):
-    try:
-        ip = ipaddress.IPv4Address(ip_to_check)
+            # if the IP addresses are past in reverse order, the function returns false
+            return False
+        # if there is no second argument, the function just checks the validity of the IP address passed to it
+        elif ipaddress.IPv4Address(first_ip):
+            # if the IP address is valid, the function returns true
+            return True
         
-        network = ipaddress.IPv4Network(subnet, strict=False)
-        if ip in network:
-                print(f"IP {ip_to_check} is within subnet {subnet}")
-                return True
-        print(f"Error: IP {ip_to_check} is not in subnet {subnet}")
-        return False
-    
+    # if the IP address is not valid, this will be caught by the except statement and the function will print the error and return false
     except ipaddress.AddressValueError as e:
         print (f"Error: Invalid IP address - {e})")
         return False
     except ValueError as e:
         print (f"Error: {e}")
         return False
-    
+        
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        if validate_ip(sys.argv[1]):
-            ping_ip(sys.argv[1]) 
-        elif validate_ip_in_subnet(sys.argv[1].split('/')[0],sys.argv[1]):
-            check_ip_subnet(sys.argv[1]) 
     
-    elif len(sys.argv) == 3:
-        if sys.argv[1] == sys.argv[2]:
-            print ('The First Argument is the same as the Second Argument')
-            print ('Skiping Second Argument')
-            if validate_ip(sys.argv[1]):
-                ping_ip(sys.argv[1])  
-        elif validate_ip_in_range(sys.argv[1], sys.argv[2]):
-            check_ip_range(sys.argv[1], sys.argv[2])
+    # create a argparse object 
+    parser = argparse.ArgumentParser(description="Ping IPs or check IP ranges/subnets.")
+    
+    # first IP or subnet is an optional argument; add the "ip_or_subnet" argument
+    # I decided to make this argument optional as well, because I want the program to print the help message if no input is provided  
+    parser.add_argument("ip_or_subnet", nargs="?", help="An IP address or subnet to ping.")
+    
+    # second IP is a optional argument (hence, nargs='?'; add the "end_ip" argument
+    parser.add_argument("end_ip", nargs="?", help="End IP address for a range (optional).")
+   
+    # retrieve the arguments supplied by the user
+    args = parser.parse_args()
+    
+    # if there is only one IP address specified:
+    if args.ip_or_subnet and not args.end_ip:
+        
+       
+        # if the first argument contains a "/" then that means we want to ping all the IPs in the subnet
+        if '/' in args.ip_or_subnet:
+            
+            # ping all IPs in the subnet 
+            check_ip_subnet(args.ip_or_subnet)
+            
+         # check if the IP address specified is valid before doing anything else
+        elif validate_ip(args.ip_or_subnet):
+            
+            # if the IP is valid, execute a ping command to the IP 
+            ping_ip(args.ip_or_subnet)
+        
+
+    # if two arguments specified 
+    elif args.ip_or_subnet and args.end_ip:
+
+        if args.ip_or_subnet == args.end_ip:
+            print('The First Argument is the same as the Second Argument')
+            print('Skipping Second Argument')
+            if validate_ip(args.ip_or_subnet):
+                ping_ip(args.ip_or_subnet)
+        
+        #check if end_ip is greater than starting ip...
+        elif(validate_ip(args.ip_or_subnet, args.end_ip)):
+            
+            # if end_ip is greater than starting ip, then check all those ips...
+            check_ip_range(args.ip_or_subnet, args.end_ip)
+            
+        
+        else: # if the end_ip is less than the starting ip, then switch end_ip with starting ip, and check all ips...
+             print("Start IP is > End IP")
+             print("Swaping Start IP and End IP")
+             check_ip_range(args.end_ip, args.ip_or_subnet)
+        
     
     else:
-        print("Usage: message..")
+        # if the user doesn't supply any input, a help message with an explanation of each argument is printed
+        parser.print_help()
